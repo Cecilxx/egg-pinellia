89bb6ebc7e24d971ebd536280a734f2bdf77b7c0
Cecilxx <xingxing.liu@ele.me>
2017-10-11 18:14:40 +0800

    npm i bcrypt: 密码加随机盐转hash

--- BE/app/service/user.js
+++ BE/app/service/user.js
@@ -1,13 +1,18 @@
+// const rand = require("csprng");
+const bcrypt = require("bcrypt");
+
 module.exports = app => {
   class UserService extends app.Service {
     async create(name, password) {
+      // 获取随机盐
+      // const salt = rand();
+      // 数据库查询当前用户名
       const sqlName = await app.mysql.get("user_info", { name });
 
       if (!name || !password) {
-        //用户名或密码为空
         return {
           code: 1,
-          msg: "请输入正确的用户名和密码"
+          msg: "无效的用户名或密码"
         };
       }
 
@@ -18,7 +23,11 @@ module.exports = app => {
         };
       }
 
-      await app.mysql.insert("user_info", { name, password });
+      const salt = await bcrypt.genSalt(10);
+
+      const hash = await bcrypt.hash(password, salt);
+
+      await app.mysql.insert("user_info", { name, salt, hash });
 
       return {
         code: 0,
