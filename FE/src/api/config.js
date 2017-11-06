d7794e5ffcea127beaf1d15afbaab48088e76fb2
Cecilxx <xingxing.liu@ele.me>
2017-11-06 18:03:24 +0800

    接口路径修改

--- FE/src/api/config.js
+++ FE/src/api/config.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default axios.create({
   // baseURL: 'http://localhost:8080',
-  baseURL: '',
+  baseURL: 'http://97.64.43.217:7001',
   transformResponse: [
     function(data) {
       const res = data && JSON.parse(data)
